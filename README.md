d63259e59350e958b5ccede6d6c2936fa557d171
Maxim Shafarostov <capestiff@ya.ru>
2017-08-26 11:18:53 +0300

    first solution

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The program gets for the input a path to file containing urls and checks its availability (HTTP 200 Status) and the payment expiration date is more than a month.
+
+# Run
+```
+Enter the path to the file, containing URLs for checking: urls.txt
+http://algomost.com HTTP 200 - OK, paid under a month -  2017-09-04 16:59:16
+```
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
-def load_urls4check(path):
-    pass
+import requests
+import whois
+import datedelta
+from pathlib import Path
+from datetime import datetime
 
-def is_server_respond_with_200(url):
-    pass
 
-def get_domain_expiration_date(domain_name):
-    pass
+def load_urls4check(path_to_file):
+    with open(path_to_file, 'r') as file:
+        url_list = file.readlines()
+    return [url_.strip('\n') for url_ in url_list]
+
+
+def is_server_respond_with_200(url_):
+    request = requests.get(url_)
+    status_code = request.status_code
+
+    if status_code == 200:
+        return True
+    else:
+        return False
+
+
+def get_domain_expiration_date(domain_url):
+    domain = whois.whois(domain_url)
+    return domain.expiration_date
+
+
+def is_file(str_input):
+    if not str_input:
+        return False
+    elif Path(str_input).is_file():
+        return True
+    else:
+        print('Please, verify the path and filename')
+        return False
 
 if __name__ == '__main__':
-    pass
+    filepath = ''
+    while not is_file(filepath):
+        filepath = input('Enter the path to the file, containing URLs for checking: ')
+
+    link_list = load_urls4check(filepath)
+
+    for url in link_list:
+        if is_server_respond_with_200(url):
+            expiration_date = get_domain_expiration_date(url)
+            if isinstance(expiration_date, list):
+                expiration_date = expiration_date[0]
+            if (expiration_date - datedelta.MONTH) > datetime.now():
+                print(url, 'HTTP 200 - OK, paid over a month - ', expiration_date)
+            else:
+                print(url, 'HTTP 200 - OK, paid under a month - ', expiration_date)
+        else:
+            print(url, 'ERROR')
